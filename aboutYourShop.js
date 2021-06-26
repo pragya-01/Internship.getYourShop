91304234d8758c5afa0ae972a6ec74c32f52fc91
Pragya Agrawal <56079032+pragya-01@users.noreply.github.com>
2021-06-26 15:39:07 +0530

    Add files via upload

---
+++ aboutYourShop.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import './aboutYourShop.css';'=='
+--
+
+
+class AboutYourShop extends Component {
+  render() {
+  return (
+          
+    <div className="aboutYourShop">
+    <p className="aboutYourShopTitle">
+        About Your Shop
+    </p>
+    <p className="aboutYourShopDescription">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rhoncus euismod quam 
+        euismod ultricies. Nunc sagittis, quam id fringilla feugiat, est ante tristique dui,
+         Cras rutrum arcu in lectus finibus, eget consectetur purus tristique. Donec 
+         tincidunt tincidunt vehicula. Ut sodales gravida lectus et tincidunt. Cras
+          vestibulum ut odio eget pretium.99 O0
+
+
+
+
+
+
+
+
+
+
+          
+    </p> //Where you will mention about your shop
+</div>
+           );
+}
+};
+
+export default AboutYourShop;
